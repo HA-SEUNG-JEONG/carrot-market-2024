af314ccda524fe87cb5f344212446ead6ba56f37
HA-SEUNG-JEONG <gktmd652@naver.com>
2024-05-18 19:26:44 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,27 +3,34 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+    async function handleSubmit(data: FormData) {
+        "use server";
+    }
+
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form className="flex flex-col gap-3">
+            <form action={handleSubmit} className="flex flex-col gap-3">
                 <FormInput
                     type="email"
                     placeholder="Email"
                     required
                     errors={[]}
+                    name="email"
                 />
                 <FormInput
                     type="password"
                     placeholder="Password"
                     required
                     errors={[]}
+                    name="password"
                 />
                 <FormButton loading={false} text="Log in" />
             </form>
+
             <SocialLogin />
         </div>
     );
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,21 +3,24 @@ interface FormInputProps {
     placeholder: string;
     required: boolean;
     errors: string[];
+    name: string;
 }
 
 export default function FormInput({
     type,
     placeholder,
     required,
-    errors
+    errors,
+    name
 }: FormInputProps) {
     return (
         <div className="flex flex-col gap-2">
             <input
-                className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+                className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 pl-2"
                 type={type}
                 placeholder={placeholder}
                 required={required}
+                name={name}
             />
             {errors.map((error, index) => (
                 <span key={index} className="text-red-500 font-medium">

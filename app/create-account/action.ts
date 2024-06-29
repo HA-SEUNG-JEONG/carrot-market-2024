25301da342c805d0e4fb027162c1bbccbd59cdc4
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-06-30 08:08:27 +0900

    Regex 적용

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -2,6 +2,10 @@
 
 import { z } from "zod";
 
+const passwordRegex = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
 const checkUsername = (username: string) => !username.includes("potato");
 
 const checkPassword = ({
@@ -21,9 +25,17 @@ const formSchema = z
             })
             .min(3, "비밀번호는 3자리 이상이어야 합니다.")
             .max(10, "비밀번호는 최대 10자리입니다.")
+            .toLowerCase()
+            .trim()
             .refine(checkUsername, "custom error"),
         email: z.string().email(),
-        password: z.string().min(10),
+        password: z
+            .string()
+            .min(4)
+            .regex(
+                passwordRegex,
+                "비밀번호는 소문자, 대문자, 숫자, 특수문자를 포함해야 합니다."
+            ),
         confirmPassword: z.string().min(10)
     })
     .refine(checkPassword, {
@@ -40,7 +52,8 @@ export const createAccount = (prevState: any, formData: FormData) => {
     };
     const result = formSchema.safeParse(data);
     if (!result.success) {
-        console.log(result.error.flatten());
         return result.error.flatten();
+    } else {
+        console.log(result.data);
     }
 };

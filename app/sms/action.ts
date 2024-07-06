069e18daea2c9800734d580d7a6f46463b756e4a
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-07-06 19:31:27 +0900

    sms validation

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -1,13 +1,50 @@
 "use server";
+import { redirect } from "next/navigation";
 import validator from "validator";
+
+// refine 메소드에는 함수가 들어가는데 조건문이어야 함
 
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+    .string()
+    .trim()
+    .refine(
+        (phone) => validator.isMobilePhone(phone, "ko-KR"),
+        "Wrong Phone Number"
+    );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
-export const smsLogin = async (prevState: any, formData: FormData) => {
-    console.log(typeof formData.get("token"));
 
-    console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+    token: boolean;
+}
+
+export const smsLogin = async (prevState: ActionState, formData: FormData) => {
+    const phone = formData.get("phone");
+    const token = formData.get("token");
+
+    if (!prevState.token) {
+        const result = phoneSchema.safeParse(phone);
+        if (!result.success) {
+            return {
+                token: false,
+                error: result.error.flatten()
+            };
+        } else {
+            return {
+                token: true
+            };
+        }
+    } else {
+        const result = tokenSchema.safeParse(token);
+        if (!result.success) {
+            return {
+                token: true,
+                error: result.error.flatten()
+            };
+        } else {
+            redirect("/");
+        }
+    }
 };
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,14 @@ import Input from "@/components/input";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./action";
 
+const initialState = {
+    token: false,
+    error: undefined
+};
+
 export default function SMSLogin() {
-    const [state, action] = useFormState(smsLogin, null);
+    const [state, action] = useFormState(smsLogin, initialState);
+
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
@@ -14,21 +20,29 @@ export default function SMSLogin() {
                 <h2 className="text-xl">Verify your phone number.</h2>
             </div>
             <form action={action} className="flex flex-col gap-3">
-                <Input
-                    name="phone"
-                    type="text"
-                    placeholder="Phone number"
-                    required
+                {state.token ? (
+                    <Input
+                        name="token"
+                        type="number"
+                        placeholder="Verification code"
+                        required
+                        min={100000}
+                        max={999999}
+                    />
+                ) : (
+                    <Input
+                        name="phone"
+                        type="text"
+                        placeholder="Phone number"
+                        required
+                        errors={state.error?.formErrors}
+                    />
+                )}
+                <Button
+                    text={
+                        state.token ? "Verify Token" : "Send Verification SMS"
+                    }
                 />
-                <Input
-                    name="token"
-                    type="number"
-                    placeholder="Verification code"
-                    required
-                    minLength={100000}
-                    maxLength={999999}
-                />
-                <Button text="Verify" />
             </form>
         </div>
     );

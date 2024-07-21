b93f2b0d609a6fede9f6f6dca18bb6def1ebb277
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-07-21 11:25:44 +0900

    RelationShip

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const test = async () => {
+    const token = await db.sMSToken.create({
+        data: {
+            token: "1212112",
+            user: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    });
+    console.log(token);
+};
+
+test();
+
+export default db;

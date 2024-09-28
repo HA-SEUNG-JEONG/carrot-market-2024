79334c7070efa60acaaf67f37187c55816a50b4e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-28 19:31:18 +0900

    Feat : Skeleton

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,9 @@
-export default function Product() {
+const getProducts = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 100000));
+};
+
+export default async function Product() {
+    const product = await getProducts();
     return (
         <div>
             <h1 className="text-white text-4xl">Product!</h1>
